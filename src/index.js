b12ff8bad43a727131fcc40d9ff3469ac5eb275b
joy <hassjoy26@gmail.com>
2025-07-23 06:26:31 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import React from 'react'
 
+function Links() {
+  return (
+    <div>
+        <h3>Links</h3>
+        <a href="{props.github}">https://github.com/liza</a>
+    <a href="{props.linkedin}">https://www.linkedin.com/in/liza/</a>
+    
+    </div>
+
+  )
+}
+
+export default Links
 ReactDOM.render(<App />, document.getElementById("root"));
